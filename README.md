6f673dfee5ebc78cb857b47c18cbb4f289be2431
kkamara <kelvinkamara@protonmail.com>
2022-10-20 18:00:50 +0100

    Add project

---
+++ README.md
@@ -0,0 +1,26 @@
+# Blockchain
+
+## Installation
+
+* [Node.js](https://nodejs.org/en/) 
+* [npm](https://www.npmjs.com/get-npm) 
+* [Yarn](https://www.yarnpkg.com)
+
+```bash
+  npm i -g yarn
+```
+
+## Usage
+
+```bash
+  yarn start
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[BSD](https://opensource.org/licenses/BSD-3-Clause)
+
---
+++ block.js
@@ -0,0 +1,48 @@
+const SHA256 = require('crypto-js/sha256')
+
+class Block {
+  constructor(index, timestamp, data, previousHash='') {
+    this.index = index
+    this.timestamp = timestamp
+    this.data = data
+    this.previousHash = previousHash
+    this.hash = ''
+    this.hash = this.calculateHash()
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index + 
+      this.previousHash + 
+      this.timestamp + 
+      JSON.stringify(this.data).toString()
+    )
+  }
+}
+
+class BlockChain{
+  constructor() {
+    this.chain = [this.createGenesisBlock()]
+  }
+
+  createGenesisBlock() {
+    return new Block (0, '01/01/2022', 'Genesis Block', 0)
+  }
+
+  getPreviousBlock() {
+    return this.chain[this.chain.length - 1]
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getPreviousBlock().hash
+    newBlock.hash = newBlock.calculateHash()
+    this.chain.push(newBlock)
+  }
+}
+
+let newCoin = new BlockChain()
+
+newCoin.addBlock(new Block(1, '23/01/2022', { amount: 200, }))
+newCoin.addBlock(new Block(1, '15/02/2022', { amount: 100, }))
+
+console.log(JSON.stringify(newCoin))
